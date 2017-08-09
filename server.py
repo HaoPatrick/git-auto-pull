cbd554995f8c0263ebf6ae968ea9e140874e20ab
haoxiangpeng <3150102255@zju.edu.cn>
2017-08-09 15:06:04 +0000

    add more error code

--- server.py
+++ server.py
@@ -11,9 +11,16 @@ def github_hook():
   if request.method == 'POST':
     # load project config according to the post data
     request_data = request.data.decode('utf-8')
-    request_data = json.loads(request_data)
-    project_name = request_data['repository']['name']
-    project_conf = config[project_name]
+    try:
+      request_data = json.loads(request_data)
+    except Exception as e:
+      return "Json parse error, are you sure you're using json?", 500
+    
+    try:
+      project_name = request_data['repository']['name']
+      project_conf = config[project_name]
+    except Exception as e:
+      return 'We do not have such repo yet', 500
     
     if project_conf['url'] != request_data['repository']['html_url']:
       return 'Not a chance', 403
@@ -23,8 +30,10 @@ def github_hook():
                          request.data, hashlib.sha1).hexdigest()
     if hmac.compare_digest(signature,
                            request.headers.get('X-Hub-Signature').split('=')[1]):
-      subprocess.call(['git', 'reset', '--hard', 'HEAD'])
-      subprocess.call(['git', '-C', project_conf['path'], 'pull', 'origin', 'master'])
+      ret_code = subprocess.call(['git', 'reset', '--hard', 'HEAD'])
+      ret_code += subprocess.call(['git', '-C', project_conf['path'], 'pull', 'origin', 'master'])
+      if ret_code != 0:
+        return 'Oops, your broken git!', 500
       return 'OK, I got it', 200
     else:
       return 'Not a chance', 403
